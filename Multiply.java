2c5577c3198a6b70bff3558b01bf3ccca322d65f
Sabal Sigdel <sigdelsabal20@gmail.com>
2025-08-20 12:39:04 +0545

    Create Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Multiply {
+public static void main(String[ ] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter two numbers :");
+       int x = sc.nextInt();
+       int y = sc.nextInt();
+       System.out.println("\nTheir product is "+(x*y));
+
+}
+
+
+
+}
