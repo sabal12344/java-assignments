3bc1afe46ea3b8a54b21536930bdbc85e99a455d
Sabal Sigdel <sigdelsabal20@gmail.com>
2025-08-20 13:43:46 +0545

    7

---
+++ Escape.java
@@ -0,0 +1,20 @@
+
+//7) Write a program to demonstrate the use of all escape sequences available in java.
+
+
+
+public class Escape {
+
+       public static void main(String[] args) {
+        System.out.println("New\nLine\n\n"); // Newline
+        System.out.println("T\tA\tB"); // Tab
+        System.out.println("This is a \"quoted\" text."); // Double quote
+        System.out.println("C:\\Users\\Sabal"); // Backslash
+        System.out.println("hee\bllo, i hope there is no tyoo\b\bpo in here.");//backspace
+        System.out.println("I dont think \r promoting violence is a good idea.");//carriage  return
+    }
+    
+
+
+
+}
